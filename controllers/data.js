37bc627f69e3ee1ddf0642fbc65cb3c6ab132bc7
nzitondafustino <74200367+HonoreCyuzuzo@users.noreply.github.com>
2021-01-04 13:48:05 +0200

    bug fix

--- controllers/data.js
+++ controllers/data.js
@@ -53,7 +53,7 @@ exports.getExportData = async function(req,res,next){
     EC:d.EC,
     temperature:d.temperature,
     VWC:d.VWC,
-    created:new Date(d.created).toISOString()
+    Date:new Date(d.created).toISOString()
     };
     csvData.push(columnData);
   });
